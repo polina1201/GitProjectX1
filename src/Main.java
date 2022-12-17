87a349b73fc631b330ba07e503c754a7c25f087d
polina1201 <polina.melvin1201@gmail.com>
2022-12-17 10:58:44 -0500

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("This is first version");
+        System.out.println("This is  first version");
     }
 }
